015a3c31814c6eb802e485031833ab4bf3c4c593
Andrey Golubenko <a.g.07.08.78@gmail.com>
2021-10-08 15:07:28 +0300

    add banner animation

--- src/components/ErrorLocation.tsx
+++ src/components/ErrorLocation.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 export const ErrorLocation: React.FC = () => {
 
     return (
-        <div className="error-location-spoiler">
-            <h5 className="location-spoiler-heading">
+        <div className="error-location-banner">
+            <h5 className="location-banner-banner">
                 Unfortunately, we could not determine your current city. </h5>
-            <p className="location-spoiler-text">
+            <p className="location-banner-text">
                 To view the current weather in your or another city, enter its name in the field below.
             </p>
         </div>
--- src/components/SuccessLocation.tsx
+++ src/components/SuccessLocation.tsx
@@ -6,14 +6,19 @@ export const SuccessLocation: React.FC = () => {
     const currentCity = useAppSelector(state => state.weather.currentLocation);
 
     return (
-        <div className="location-spoiler">
-            <h5 className="location-spoiler-heading">
-                Your current city is <b>{currentCity || 'undefined'}</b> and
-                you {currentCity ? 'can' : 'can\'t'} view current weather in it below.
-            </h5>
-            <p className="location-spoiler-text">
-                Also, to view the current weather in another city, enter its name in the field below.
-            </p>
-        </div>
+        <>
+            {
+                currentCity &&
+                <div className="location-banner">
+                    <h5 className="location-banner-heading">
+                        Your current city is <b>{currentCity || 'undefined'}</b> and
+                        you {currentCity ? 'can' : 'can\'t'} view current weather in it below.
+                    </h5>
+                    <p className="location-banner-text">
+                        Also, to view the current weather in another city, enter its name in the field below.
+                    </p>
+                </div>
+            }
+        </>
     )
 };
