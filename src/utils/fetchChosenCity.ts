f53f6c2c0d4395d13370b301ef005d851cbe2596
Andrey-Golubenko <andrey.golubenko.it@gmail.com>
2023-08-23 15:34:04 +0200

    small fix

--- src/utils/fetchChosenCity.ts
+++ src/utils/fetchChosenCity.ts
@@ -3,7 +3,7 @@ import {API_KEY, API_URL} from '../config'
 export async function fetchChosenCity(cityName: string) {
 
     const response = await fetch(`${API_URL}weather?q=${cityName}&appid=${API_KEY}`);
-    if (!response.ok && response.status !== 404) {
+    if (!response.ok && response.status === 404) {
         throw new Error('Server responded with an error')
     }
     const data = await response.json();
